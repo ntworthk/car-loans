78e8ff6bc4374de40338ed4c19b8aa0c6f381f1b
Nick Twort <nicholas.twort@gmail.com>
2023-03-30 17:47:20 +1100

    Let's scrape some avos

---
+++ source/scrape-avos.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(httr)
+
+res <- httr::GET(url = "https://www.woolworths.com.au/")
+
+cookies <- cookies(res)
+tmp_cookies <- cookies$value
+names(tmp_cookies) <- cookies$name
+cookies <- tmp_cookies
+cookies["bff_region"] <- "syd2"
+
+headers = c(
+  `authority` = "www.woolworths.com.au",
+  `accept` = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  `accept-language` = "en-AU,en-NZ;q=0.9,en-GB;q=0.8,en-US;q=0.7,en;q=0.6",
+  `cache-control` = "max-age=0",
+  `dnt` = "1",
+  `sec-ch-ua` = '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+  `sec-ch-ua-mobile` = "?0",
+  `sec-ch-ua-platform` = '"Windows"',
+  `sec-fetch-dest` = "document",
+  `sec-fetch-mode` = "navigate",
+  `sec-fetch-site` = "none",
+  `sec-fetch-user` = "?1",
+  `upgrade-insecure-requests` = "1",
+  `user-agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+)
+
+res <- httr::GET(url = "https://www.woolworths.com.au/apis/ui/product/detail/186910", httr::add_headers(.headers=headers), set_cookies(cookies))
+
+avos <- content(res)
+
+write_rds(avos, file.path("data", paste0("avos_", Sys.Date(), ".rds")))
+
+
