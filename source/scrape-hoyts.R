faebcafd9a89da123ac81a0a0bb71b571b0be5e3
Nick Twort <nick.twort@houstonkemp.com>
2024-08-12 16:13:31 +1000

    Add Hoyts movie list

---
+++ source/scrape-hoyts.R
@@ -0,0 +1,33 @@
+library(httr)
+library(tidyverse)
+library(arrow)
+
+res <- GET(
+  url = "https://apim.hoyts.com.au/au/cinemaapi/api/movies/now-showing"
+)
+
+movies <- content(res)
+
+movies <- movies |> 
+  map_dfr(
+    function(movie) {
+      movie |> 
+        compact() |> 
+        (\(x) {
+          x$attribute <- list(x$attribute)
+          x$genres <- list(x$genres)
+          x
+        })() |> 
+        as_tibble_row()
+    }
+  ) |> 
+  mutate(timestamp = Sys.time())
+
+
+concat_tables(
+  read_parquet("data/hoyts_now_showing.parquet", as_data_frame = FALSE),
+  arrow_table(movies)
+) |> 
+  write_parquet(sink = "data/hoyts_now_showing_temp.parquet")
+
+file.rename("data/hoyts_now_showing_temp.parquet", "data/hoyts_now_showing.parquet")
